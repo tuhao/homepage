5f8d4713299612a3b36b7da06e44a07fed342429
unknown <astro.liuhang@gmail.com>
2013-11-20 18:02:02 +0800

    accounts ignore

--- users/views.py
+++ users/views.py
@@ -18,45 +18,45 @@ def index(request):
     return render_to_response('index.html')
 
 
-def login(request):
-    username = ''
-    message = []
-    if request.method == 'POST':
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            request.session['user'] = user
-            return render_to_response('index.html', context_instance=RequestContext(request))
-        else:
-            message.append('username or password not correct')
-    else:
-        user = request.session.get('user', None)
-        if user and user.is_authenticated():
-            return render_to_response('index.html', context_instance=RequestContext(request))
-    return render_to_response('login.html', {'username': username, 'message': ''.join(message)}, context_instance=RequestContext(request))
+#def login(request):
+#    username = ''
+#    message = []
+#    if request.method == 'POST':
+#        username = request.POST.get('username', '')
+#        password = request.POST.get('password', '')
+#        user = auth.authenticate(username=username, password=password)
+#        if user is not None:
+#            auth.login(request, user)
+#            request.session['user'] = user
+#            return render_to_response('index.html', context_instance=RequestContext(request))
+#        else:
+#            message.append('username or password not correct')
+#    else:
+#        user = request.session.get('user', None)
+#        if user and user.is_authenticated():
+#            return render_to_response('index.html', context_instance=RequestContext(request))
+#    return render_to_response('login.html', {'username': username, 'message': ''.join(message)}, context_instance=RequestContext(request))
+
+#
+#def logout(request):
+#    auth.logout(request)
+#    request.session['user'] = None
+#    return render_to_response('login.html', context_instance=RequestContext(request))
 
 
-def logout(request):
-    auth.logout(request)
-    request.session['user'] = None
-    return render_to_response('login.html', context_instance=RequestContext(request))
-
-
-def regist(request):
-    if request.method == 'POST':
-        form = RegistForm(data=request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            new_user = User.objects.create_user(
-                username=cd['username'], email=cd['email'], password=cd['password'])
-            new_user.is_staff = False
-            new_user.save()
-            return render_to_response("login.html", {'username': cd['username']})
-        else:
-            return render_to_response("regist.html", {'username': request.POST.get('username'), 'email': request.POST.get('email'), 'message': form.errors}, context_instance=RequestContext(request))
-    return render_to_response("regist.html", context_instance=RequestContext(request))
+#def regist(request):
+#    if request.method == 'POST':
+#        form = RegistForm(data=request.POST)
+#        if form.is_valid():
+#            cd = form.cleaned_data
+#            new_user = User.objects.create_user(
+#                username=cd['username'], email=cd['email'], password=cd['password'])
+#            new_user.is_staff = False
+#            new_user.save()
+#            return render_to_response("login.html", {'username': cd['username']})
+#        else:
+#            return render_to_response("regist.html", {'username': request.POST.get('username'), 'email': request.POST.get('email'), 'message': form.errors}, context_instance=RequestContext(request))
+#    return render_to_response("regist.html", context_instance=RequestContext(request))
 
 
 def get_client_ip(request):
