87b40a77c0f1d5f6ac8079b4244d88a950e2438f
yasir <astro.liuhang@gmail.com>
2013-11-21 06:49:58 +0800

    paginate

--- blog/views.py
+++ blog/views.py
@@ -5,49 +5,73 @@ from django.template import RequestContext
 from blog.models import *
 from django.db.models import Count
 
-pagenum = 10
+PAGE_SIZE = 2
 
 
 def start_index(page):
     if page <= 0:
         return 0
-    return (int(page) - 1) * pagenum
+    return (int(page) - 1) * PAGE_SIZE
 
 
 def end_index(page):
-    return int(page) * pagenum
+    return int(page) * PAGE_SIZE
+
+
+def max_page(total_size):
+    total = int(total_size)
+    if total % PAGE_SIZE == 0:
+        return total / PAGE_SIZE
+    else:
+        return (total - (total % PAGE_SIZE)) / PAGE_SIZE + 1
+
+
+def validate(page, total):
+    if int(page) > max_page(total):
+        return max_page(total)
+    if int(page) < 1:
+        return 1
+    return page
+
+
+def sortp(sort_page):
+    sort_total = Sort.objects.count()
+    sp = validate(sort_page, sort_total)
+    return start_index(sp), end_index(sp)
+
+
+def blogp(blog_page):
+    blog_total = Blog.objects.count()
+    bp = validate(blog_page, blog_total)
+    return start_index(bp), end_index(bp)
 
 
 def blogs(request, sort_page=1, blog_page=1):
-    try:
-        blogs = Blog.objects.order_by('pub_date')[
-            start_index(blog_page):end_index(blog_page)]
-        sorts = Sort.objects.annotate(blog_count=Count('blog')).order_by(
-            'id')[start_index(sort_page):end_index(sort_page)]
-    except Exception, e:
-        sorts = Sort.objects.annotate(blog_count=Count('blog')).order_by(
-            'id')[0:pagenum]
-        blogs = Blog.objects.order_by('pub_date')[0:pagenum]
-    blog_total = Blog.objects.count()
+    sp = sortp(sort_page)
+    bp = blogp(blog_page)
+    blogs = Blog.objects.order_by('pub_date')[
+        bp[0]:bp[1]]
+    sorts = Sort.objects.annotate(
+        blog_count=Count('blog')).order_by('id')[sp[0]:sp[1]]
     return render_to_response("blog_list.html", locals(), context_instance=RequestContext(request))
 
 
-def blog_detail(request, blog_id, sort_id,blog_page=1,sort_page=1):
+def blog_detail(request, blog_id, sort_id, blog_page=1, sort_page=1):
+    sp = sortp(sort_page)
+    bp = blogp(blog_page)
     sorts = Sort.objects.annotate(
-        blog_count=Count('blog')).order_by('id')[start_index(sort_page):end_index(sort_page)]
+        blog_count=Count('blog')).order_by('id')[sp[0]:sp[1]]
     sort = get_object_or_404(Sort, pk=sort_id)
-    blogs = Blog.objects.filter(sort=sort)[
-        start_index(blog_page):end_index(blog_page)]
-    blog_total = Blog.objects.count()
+    blogs = Blog.objects.filter(sort=sort)[bp[0]:bp[1]]
     blog = get_object_or_404(Blog, pk=blog_id)
     return render_to_response("blog_detail.html", locals(), context_instance=RequestContext(request))
 
 
 def sort_blogs(request, sort_id, blog_page=1, sort_page=1):
+    sp = sortp(sort_page)
+    bp = blogp(blog_page)
     sorts = Sort.objects.annotate(
-        blog_count=Count('blog')).order_by('id')[start_index(sort_page):end_index(sort_page)]
+        blog_count=Count('blog')).order_by('id')[sp[0]:sp[1]]
     sort = get_object_or_404(Sort, pk=sort_id)
-    blogs = Blog.objects.filter(sort=sort)[
-        start_index(blog_page):end_index(blog_page)]
-    blog_total = Blog.objects.count()
+    blogs = Blog.objects.filter(sort=sort)[bp[0]:bp[1]]
     return render_to_response("blog_sort.html", locals(), context_instance=RequestContext(request))
