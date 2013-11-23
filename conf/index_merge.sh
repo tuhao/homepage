965e078c68a07db4ca0a72e90f1f85f713a3d41d
root <astro.liuhang@gmail.com>
2013-11-23 12:18:49 +0000

    deploy

--- conf/index_merge.sh
+++ conf/index_merge.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+#initialization
+#mkdir -p /usr/local/etc/log
+#mkdir -p /usr/local/etc/data/blog_index
+#mkdir -p /usr/local/etc/data/delta
+#indexer blog_index
+#indexer delta
+
+#update index
 indexer delta --rotate
 indexer --merge blog_index delta --merge-dst-range deleted 0 0 --rotate
